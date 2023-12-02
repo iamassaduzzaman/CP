9e90c2671b9b7dce0921076533c77b038b02acdc
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2023-12-02 23:23:11 +0600

    constructive

---
+++ P-01/C-04/constructive_2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int test; cin >> test;
+    while(test--){
+        int x; cin >> x;
+        cout << "1" << " " << x-1 << '\n';
+    }
+    return 0;
+}
