8ed717d603bbb81dd1a4af13019d2bd211b072c4
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2024-01-08 09:35:26 +0600

    pbds

---
+++ P-01/Custom/pbds.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ordered_set os;
+   
+    return 0;
+}
