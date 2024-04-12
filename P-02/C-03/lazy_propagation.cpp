31a3f99555ddf1becee4ff3eecd75d32bbb1a6cc
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2024-04-12 06:07:40 +0600

    upd on lazy propagation

--- P-02/C-03/lazy_propagation.cpp
+++ P-02/C-03/lazy_propagation.cpp
@@ -2,82 +2,85 @@
 using namespace std;
 
 const int N = 1e5 + 9;
+using ll = long long;
 int a[N];
-struct node {
-	int sum = 0;
-	int prop = 0;
-} tree[N * 4];
-
-// build
-void build(int node, int b, int e) {
-	if (b == e) {
-		tree[node].sum = a[b];
+ll t[N * 4], lazy[N * 4];
+// push
+void push(int n, int b, int e) {
+	if (lazy[n] == 0) {
 		return;
 	}
-	int left = node * 2;
-	int right = node * 2 + 1;
-	int mid = (b + e) / 2;
-	build(left, b, mid);
-	build(right, mid + 1, e);
-	tree[node].sum = tree[left].sum + tree[right].sum;
-}
 
+	t[n] = t[n] + 1LL * (e - b + 1) * lazy[n];
+
+	if (b != e) {
+		int l = 2 * n, r = 2 * n + 1;
+		lazy[l] += lazy[n];
+		lazy[r] += lazy[n];
+	}
+
+	lazy[n] = 0;
+}
+// build
+void build(int n, int b, int e) {
+	if (b == e) {
+		t[n] = a[b];
+		return;
+	}
+	int l = 2 * n, r = 2 * n + 1, mid = (b + e) / 2;
+	build(l, b, mid);
+	build(r, mid + 1, e);
+	t[n] = t[l] + t[r];
+}
 // update
-void update(int node, int b, int e, int i, int j, int value) {
-	// entirely disjoint
+void upd(int n, int b, int e, int i, int j, int v) {
+	push(n, b, e);
 	if (i > e or j < b) {
 		return;
 	}
-	// convers you
 	if (b >= i and e <= j) {
-		tree[node].sum += ((e - b + 1) * value);
-		tree[node].prop += value;
+		lazy[n] = v;
+		push(n, b, e);
 		return;
 	}
-	// everything else
-	int left = node * 2;
-	int right = node * 2 + 1;
-	int mid = (b + e) / 2;
-	update(left, b, mid, i, j, value);
-	update(right, mid + 1, e, i, j, value);
-	tree[node].sum = tree[left].sum + tree[right].sum + ((e - b + 1) * tree[node].prop);
+	int l = 2 * n, r = 2 * n + 1, mid = (b + e) / 2;
+	upd(l, b, mid, i, j, v);
+	upd(r, mid + 1, e, i, j, v);
+	t[n] = t[l] + t[r];
 }
-
 // query
-int query(int node, int b, int e, int i, int j, int carry) {
-	// entirely disjoint
+ll query(int n, int b, int e, int i, int j) {
+	push(n, b, e);
 	if (i > e or j < b) {
 		return 0;
 	}
-	// covers you 
 	if (b >= i and e <= j) {
-		return tree[node].sum + carry * (e - b + 1);
+		return t[n];
 	}
-	// everything else
-	int left = node << 1;
-	int right = (node << 1) + 1;
-	int mid = (b + e) >> 1;
-
-	int p1 = query(left, b, mid, i, j, carry + tree[node].prop);
-	int p2 = query(right, mid + 1, e, i, j, carry + tree[node].prop);
-
-	return p1 + p2;
+	int l = 2 * n, r = 2 * n + 1, mid = (b + e) / 2;
+	return query(l, b, mid, i, j) + query(r, mid + 1, e, i, j);
 }
 
 
-int main() {
+int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	int n = 5;
-	for (int i = 1; i <= n; ++i) {
-		a[i] = i;
-	}
-
+	int n, q; cin >> n >> q;
 	build(1, 1, n);
-	cout << query(1, 1, n, 1, 5, 0) << '\n';
-	update(1, 1, n, 2, 4, 2);
-	update(1, 1, n, 4, 5, 3);
-	cout << query(1, 1, n, 3, 5, 0) << '\n';
+	while (q--) {
+		int ty; cin >> ty;
+		if (ty == 1) {
+			int l, r, v; cin >> l >> r >> v;
+			++l;
+			upd(1, 1, n, l, r, v);
+		}
+		else {
+			int i; cin >> i;
+			++i;
+			cout << query(1, 1, n, i, i) << '\n';
+		}
+	}
+    
+    
 	return 0;
 }
